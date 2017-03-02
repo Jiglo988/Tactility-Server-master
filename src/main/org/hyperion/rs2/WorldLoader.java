151cfb798d2a948ea7df9d51a271df6b9d41ef94
Jesse988 <Jesse988@users.noreply.github.com>
2017-03-02 15:59:10 -0800

    Delete WorldLoader.java

--- src/main/org/hyperion/rs2/WorldLoader.java
+++
@@ -1,9 +0,0 @@
-package org.hyperion.rs2;
-
-import org.hyperion.rs2.model.Player;
-import org.hyperion.rs2.model.PlayerDetails;
-
-public interface WorldLoader {
-	LoginResponse checkLogin(Player player, PlayerDetails playerDetails);
-	boolean savePlayer(Player player);
-}
