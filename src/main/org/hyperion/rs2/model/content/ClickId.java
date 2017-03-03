5b7802d0092c4a7a3b46bf383b51369b67a54fc7
Jesse988 <Jesse988@users.noreply.github.com>
2017-03-02 16:38:34 -0800

    Delete ClickId.java

--- src/main/org/hyperion/rs2/model/content/ClickId.java
+++
@@ -1,6 +0,0 @@
-package org.hyperion.rs2.model.content;
-
-public class ClickId {
-	public static final int FIGHT_PITS_DEATH = 31000, ATTACKABLE = 30000;
-	public static final int CAN_TELEPORT = 30001;
-}
