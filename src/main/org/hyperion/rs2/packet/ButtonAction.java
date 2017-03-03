6cd904352e98e6d690f6ffa790c7358e6eddfc88
Jesse988 <Jesse988@users.noreply.github.com>
2017-03-02 16:09:45 -0800

    Delete ButtonAction.java

--- src/main/org/hyperion/rs2/packet/ButtonAction.java
+++
@@ -1,9 +0,0 @@
-package org.hyperion.rs2.packet;
-
-import org.hyperion.rs2.model.Player;
-
-public interface ButtonAction {
-	default boolean handle(final Player player, int id) {
-		return false;
-	}
-}
